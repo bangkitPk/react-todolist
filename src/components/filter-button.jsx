f9e7f49af74c89acede78b6a009434a17200a217
bangkit <bangkitputrakristana@gmail.com>
2023-10-28 21:15:02 +0700

    create filter todos feature

--- src/components/filter-button.jsx
+++ src/components/filter-button.jsx
@@ -1,7 +1,14 @@
-export default function FilterButton({ text }) {
-    return (
-        <button className="px-3 py-[.1em] bg-gray-600 rounded-2xl text-white">
-          {text}
-        </button>
-    )
+export default function FilterButton({ text, selectedFilter, onClick }) {
+  let isActive = text == selectedFilter;
+
+  return (
+    <button
+      className={`px-3 py-[.1em] rounded-2xl text-white ${
+        isActive ? "bg-teal-600" : "bg-gray-600"
+      }`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
--- src/components/list-todos.jsx
+++ src/components/list-todos.jsx
@@ -1,29 +1,61 @@
 import { useDispatch, useSelector } from "react-redux";
 import FilterButton from "./filter-button";
 import { getTodo } from "../redux/reducers/todo-reducers";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Todo from "./todo";
 
 export default function ListTodos() {
   const dispatch = useDispatch();
   const { todos } = useSelector((state) => state.todo);
+  const [selectedFilter, setSelectedFilter] = useState("ALL"); // Default filter
+  const [todosFiltered, setTodosFiltered] = useState([]);
 
   useEffect(() => {
     dispatch(getTodo());
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    filterTodos();
+  }, [selectedFilter, todos]);
+
+  const filterTodos = () => {
+    let filteredTodos = todos.filter((todo) => {
+      if(selectedFilter == "ALL"){
+        return true;
+      }else if(selectedFilter == "ACTIVE"){
+        return !todo.status;
+      }else if(selectedFilter == "COMPLETED"){
+        return todo.status;
+      }
+    })
+    setTodosFiltered(filteredTodos);
+  }
 
   return (
     <div className="w-full">
       <div className="flex justify-start gap-5">
-        <FilterButton text={"ALL"} />
-        <FilterButton text={"ACTIVE"} />
-        <FilterButton text={"COMPLETED"} />
+        <FilterButton
+          text={"ALL"}
+          selectedFilter={selectedFilter}
+          onClick={() => setSelectedFilter("ALL")}
+        />
+        <FilterButton
+          text={"ACTIVE"}
+          selectedFilter={selectedFilter}
+          onClick={() => setSelectedFilter("ACTIVE")}
+        />
+        <FilterButton
+          text={"COMPLETED"}
+          selectedFilter={selectedFilter}
+          onClick={() => setSelectedFilter("COMPLETED")}
+        />
       </div>
       <div>
-        {
-          todos.length == 0 ? <p className="mt-5">There's no to-do list yet.</p> :
-          todos.map(todo => (<Todo key={todo.id} todo={todo} />))
-        }
+        {todos.length == 0 || todosFiltered.length == 0 ? (
+          <p className="mt-5">There's no to-do list yet.</p>
+        ) : (
+          todosFiltered.map((todo) => <Todo key={todo.id} todo={todo} />)
+        )}
       </div>
     </div>
   );
